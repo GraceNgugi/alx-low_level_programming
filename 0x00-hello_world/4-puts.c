004a28032e3f5e077cb26a13a36785d718f9ffe9
GraceNgugi <gracengugi098@gmail.com>
2023-09-07 09:19:35 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
-/** Write a C program that prints exactly Programming is like building a multilingual puzzle, followed by a new line.Use the function puts **/
 #include<stdio.h>
 /**
- * int main(void)means the function takes no argument.
- * stdio.h header allows us to perform output and input operation in c.
- *\ escape character used to read the special character.
+ *main -serves as an entry point for program execution
+ *Return:0 (success)
  */
 int main(void)
-	{
-		/**puts function used to display output.**/
-		puts("\"Programming is like building a multilingual puzzle");
-		/** Return0: shows success.**/
-		return (0);
-	}
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
