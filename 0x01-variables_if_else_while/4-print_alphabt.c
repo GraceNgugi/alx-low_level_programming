415046d042d5750d73da4ccdb71880082a5b82bc
GraceNgugi <gracengugi098@gmail.com>
2023-09-08 13:27:41 -0700

    Code format

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ *main - Entry point
+ *Description: 'Print alphabet lowercase except q and e'
+ *Return: 0 (success)
+ */
+int main(void)
+{
+	int a = 97;
+
+	while (a <= 122)
+	{
+		if (a == 101 || n == 113)
+		{
+			a++;
+			continue;
+		}
+	putchar(a);
+	a++;
+	}
+	putchar('\n');
+	return (0);
+}
