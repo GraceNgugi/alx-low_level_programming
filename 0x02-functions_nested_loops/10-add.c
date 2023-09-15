e865977801b96374a76fe797d6588371e6a43a3e
GraceNgugi <gracengugi098@gmail.com>
2023-09-14 23:20:45 -0700

    a function that adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *add - adds two integers and returns the result
+ *@a: is the character to be checked
+ *@b: is the character to be checked
+ *Return: the last digit
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,7 +13,7 @@ void times_table(void)
 		for (b = 0; b <= 9; b++)
 		{
 			c = a * b;
-			if (b > 8)
+			if (b > 8 && c < 9)
 			{
 				_putchar((c % 10) + '0');
 				_putchar('\n');
@@ -21,8 +21,8 @@ void times_table(void)
 			else if (b > 8 && c > 9)
 			{
 				_putchar((c / 10) + '0');
-				_putchar((c % 10) + '0');
-                                _putchar('\n');
+				_putchar((c % 10) + '0);
+				_putchar('\n');
                         }
 			else if (c < 10)
 			{
