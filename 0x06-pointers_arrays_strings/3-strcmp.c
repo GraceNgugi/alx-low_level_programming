9b931ca1ba9028473d5ec1dda2205906e5667a49
GraceNgugi <gracengugi098@gmail.com>
2023-09-20 00:50:29 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_strcmp - a function that compares two strings
+ *@s1: string to be checked
+ *@s2: string to be checked
+ *Return: gives the comparison value
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int a, compare_value;
+
+	a = 0;
+	while (s1[a] == s2[a] &&  s1[a] != '\n')
+	{
+		a++;
+	}
+	compare_value = s1[a] - s2[a];
+	return (compare_value);
+}
