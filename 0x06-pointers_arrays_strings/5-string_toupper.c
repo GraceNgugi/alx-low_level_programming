dccdc0a430bc04b074db87712e7c7696341b57ab
GraceNgugi <gracengugi098@gmail.com>
2023-09-20 23:49:43 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *string_toupper -  a function that changes all lowercase letters
+ * of a string to uppercase
+ *@x: Pointer to string
+ *Return: The value to be returned
+ */
+char *string_toupper(char *x)
+{
+	int length;
+
+	length = 0;
+	while (length != '\0')
+	{
+		if (x[length] >= 97 && x[length] <= 122)
+		{
+			x[length] = x[length] - 32;
+		}
+		length++;
+	}
+	return (x);
+}
